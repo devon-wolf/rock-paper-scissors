b25c7f463c05c1f223348a7b7eb7093b77c3ef72
Dylan White <dylan.whitej@gmail.com>
2021-01-14 17:23:26 -0800

    everything

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import {
 } from './gameUtils.js';
 
 import {
+    displayOutcome,
+    resetResultsBox,
     updateStats
 } from './displayUtils.js';
 
@@ -12,9 +14,9 @@ const submitButton = document.getElementById('submit-button');
 const resetButton = document.getElementById('reset-button');
 	
 // display areas
-const resultsSection = document.querySelector('.results');
-const computerThrowDisplay = document.getElementById('computer-throw');
-const outcomeDisplay = document.getElementById('outcome');
+// const resultsSection = document.querySelector('.results');
+// const computerThrowDisplay = document.getElementById('computer-throw');
+// const outcomeDisplay = document.getElementById('outcome');
 
 // initialize state
 let winCount = 0;
@@ -26,27 +28,15 @@ submitButton.addEventListener('click', () => {
     const playerChoice = document.querySelector('input[type="radio"]:checked');
 	
     const playerThrow = playerChoice.value;
-    const computerThrow = getComputerThrow();
-	
-    resultsSection.classList.remove('none');
-    computerThrowDisplay.textContent = computerThrow;
-	
+    const computerThrow = getComputerThrow();   
     const outcome = doesUserWin(playerThrow, computerThrow);
-	
-    if (outcome === 'win') {
-        winCount++;
-        outcomeDisplay.textContent = 'You win!';
-    }
-    if (outcome === 'loss') {
-        loseCount++;
-        outcomeDisplay.textContent = 'You lost.';
-    }
-    if (outcome === 'draw') {
-        drawCount++;
-        outcomeDisplay.textContent = 'It\'s a draw.';
-    }
-  
+    console.log(outcome);
+    if (outcome === 'win') winCount++;  
+    if (outcome === 'loss') loseCount++;
+    if (outcome === 'draw') drawCount++;
+
     updateStats(winCount, loseCount, drawCount);
+    displayOutcome(outcome, computerThrow);
 });
 
 // reset button
@@ -58,9 +48,10 @@ resetButton.addEventListener('click', () => {
     winCount = 0;
     loseCount = 0;
     drawCount = 0;
+    resetResultsBox();
     updateStats(winCount, loseCount, drawCount);
-	
     resetDisplay.classList.remove('none');
     resetCount++;
     resetCounter.textContent = resetCount;
 });
+
--- gameUtils.js
+++ gameUtils.js
@@ -1,12 +1,25 @@
+const computerShape = document.getElementById('computer-shape'); 
+
 export function getComputerThrow() {
     return computerThrow(computerThrowCriteria());
 }
 
 // only exports to the tests
 export function computerThrow(value) {
-    if (value === 1) return 'rock';
-    else if (value === 2) return 'paper';
-    else return 'scissors';
+    computerShape.removeAttribute('class');
+    if (value === 1){
+        computerShape.classList.add('rock');
+        computerShape.textContent = 'ROCK';
+        return 'rock';
+    } else if (value === 2){
+        computerShape.classList.add('paper');
+        computerShape.textContent = 'PAPER';
+        return 'paper';
+    } else {
+        computerShape.classList.add('scissors');
+        computerShape.textContent = 'SCISSORS';
+        return 'scissors';
+    }
 }
 
 function computerThrowCriteria() {
